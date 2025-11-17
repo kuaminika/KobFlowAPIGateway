a00dcb83fe6e8cfc9bacc1b6816191e5ce2ce9ba
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2025-11-17 07:02:31 +0000

    added use statement

--- API/auth/index.php
+++ API/auth/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once dirname(__FILE__)."/../../K_Utilities/_requireAll.php";
-
+use K_Utilities\KCurlTool;
 // api/auth/index.php
 // header('Content-Type: application/json');
 // header("Access-Control-Allow-Origin: *");
